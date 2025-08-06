29152871f9ba70263c0864e6c0239a9786255fb2
Nikki Matsubara <masktu0209@gmail.com>
2025-08-06 15:02:15 +0900

    g

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 // pages/_app.tsx
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import Layout from '../components/Layout';
@@ -14,23 +15,30 @@ function MyApp({ Component, pageProps }: AppProps) {
   // ★★★ 改善点 1: HomePageの状態を_app.tsxで管理 ★★★
   const [reviewState, setReviewState] = useState<ReviewState>('idle');
 
-  const noLayoutPaths = ['/login', '/signup','/'];
+  const noLayoutPaths = ['/login', '/signup', '/'];
   const shouldApplyLayout = !noLayoutPaths.includes(router.pathname);
 
   // HomePageの場合のみ、stateをLayoutとComponentに渡す
   // Component.name を使って現在のページコンポーネントを判定
   const isHomePage = Component.name === 'HomePage';
 
-  return shouldApplyLayout ? (
-    <Layout reviewState={isHomePage ? reviewState : undefined}>
-      <Component 
-        {...pageProps} 
-        // HomePageにだけsetReviewState関数を渡す
-        setReviewStateForLayout={isHomePage ? setReviewState : undefined}
-      />
-    </Layout>
-  ) : (
-    <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {shouldApplyLayout ? (
+        <Layout reviewState={isHomePage ? reviewState : undefined}>
+          <Component
+            {...pageProps}
+            // HomePageにだけsetReviewState関数を渡す
+            setReviewStateForLayout={isHomePage ? setReviewState : undefined}
+          />
+        </Layout>
+      ) : (
+        <Component {...pageProps} />
+      )}
+    </>
   );
 }
 
