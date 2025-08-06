b4212941f1d7d74aec4571700a39d205a97b8fec
Nikki Matsubara <masktu0209@gmail.com>
2025-08-06 15:22:31 +0900

    j

--- pages/index.tsx
+++ pages/index.tsx
@@ -296,8 +296,10 @@ const LandingPage: NextPage = () => {
                     <FinalCtaSection />
                 </main>
                 <footer className="bg-slate-900 border-t border-slate-800">
-                    <Link href="/privacy-policy" legacyBehavior><a className="text-center mex-auto text-sm px-6 py-8  text-gray-500 hover:underline">プライバシーポリシー</a></Link>
-                    <div className="container mx-auto px-6 py-8 text-center text-[#8892B0] text-sm"><p> {new Date().getFullYear()} Sync Words</p></div>
+                    <div className="container mx-auto px-6 py-8 text-center text-[#8892B0] text-sm">
+                        <Link href="/privacy-policy" legacyBehavior><a className="text-gray-500 hover:underline">プライバシーポリシー</a></Link>
+                        <p> {new Date().getFullYear()} Sync Words</p>
+                    </div>
                 </footer>
             </div>
         </>
