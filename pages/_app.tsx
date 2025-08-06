d9a9464f47dc759a8c8bd46df5c4d1b5c0100adc
Nikki Matsubara <masktu0209@gmail.com>
2025-08-06 15:06:18 +0900

    g

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,7 +25,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
       </Head>
       {shouldApplyLayout ? (
         <Layout reviewState={isHomePage ? reviewState : undefined}>
