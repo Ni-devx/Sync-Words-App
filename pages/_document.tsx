9833ef54e9d05a5753e9e3135fc8ee8c3eedfdb7
Nikki Matsubara <masktu0209@gmail.com>
2025-08-07 09:36:00 +0900

    pl

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,9 @@ function Document() {
     <Html lang="ja"> {/* lang属性などもここで設定できます */}
       <Head>
         {/* ここにfaviconのリンクを記述します */}
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </Head>
       <body>
         <Main />
